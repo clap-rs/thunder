631b04643bfccd3b3504f1dff56a393efec636c5
Katharina Fey <kookie@spacekookie.de>
2018-03-28 15:47:25 +0200

    Renaming the main example to something more...THUNDEROUS

---
+++ examples/thor.rs
@@ -0,0 +1,20 @@
+//! Thor is the god of thunder
+
+#![feature(proc_macro)]
+
+extern crate clap;
+extern crate thunder;
+use thunder::thunderclap;
+
+struct MyApp;
+
+#[thunderclap]
+impl MyApp {
+    fn hello(name: String) {
+        println!("Hello World!");
+    }
+}
+
+fn main() {
+    MyApp::start();
+}
